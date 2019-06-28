6265304ab138dbdfa83b4d48278d373f6bda968c
Jovan <jovancarlleroux@msn.com>
2019-06-28 15:57:12 +0200

    Added slideout menu for mobile

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 window.addEventListener("load",function(){
-    var nodes = document.querySelectorAll('nav[data-action="expand"] *:first-child');
+    var nodes = document.querySelectorAll('nav[data-action="expand"] *:nth-child(2)');
     for(var i=0; i<nodes.length; i++){
     nodes[i].addEventListener("click",function(){
     if(this.parentNode.className == "open")this.parentNode.className = "";
